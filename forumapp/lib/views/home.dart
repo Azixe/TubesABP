1891261b92777d0d3d27931c5352ac825691730c
Dawis <arifkurniawan10tkj@gmail.com>
2025-04-26 20:48:02 +0700

    add home page

---
+++ forumapp/lib/views/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'widgets/post_field.dart';
+import 'widgets/post_data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _postController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forum APP'),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PostField(
+              hintText: 'what do you want to ask?',
+              controller: _postController,
+            ),
+            // const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                elevation: 0,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 10,
+                ),
+              ),
+              onPressed: () {},
+              child: const Text('Post'),
+            ),
+            const SizedBox(height: 30),
+            Text('Posts'),
+            const SizedBox(height: 20),
+            PosttData(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ forumapp/lib/views/widgets/post_data.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PosttData extends StatelessWidget {
+  const PosttData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Precios Oladele', style: GoogleFonts.poppins()),
+          Text('ahduahduah.com', style: GoogleFonts.poppins(fontSize: 10)),
+          const SizedBox(height: 10),
+          Text('I am a Black Hat'),
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.thumb_up)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.message)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
