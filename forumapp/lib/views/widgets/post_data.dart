84579d1656b0689ae3c11b972b76052d439a3c9f
Lebibir <Farrelrajend@Gmail.com>
2025-04-29 11:58:19 +0700

    Merge branch

--- forumapp/lib/views/widgets/post_data.dart
+++ forumapp/lib/views/widgets/post_data.dart
@@ -54,13 +54,8 @@ class PostData extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-<<<<<<< HEAD
           Text('John Doe', style: GoogleFonts.poppins()),
           Text('johndoe.com', style: GoogleFonts.poppins(fontSize: 10)),
-=======
-          Text('Farelia rajendrae', style: GoogleFonts.poppins()),
-          Text('rajendrae99.com', style: GoogleFonts.poppins(fontSize: 10)),
->>>>>>> f388ebb9173b8d2246b0e53455bd05c551c28a95
           const SizedBox(height: 10),
           Text('I like cats!'),
           Row(
